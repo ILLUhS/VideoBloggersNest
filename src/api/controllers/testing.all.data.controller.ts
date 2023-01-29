a4fa945d787f0cafdc9a7699eae9680e3b3439cc
ILLUhS <illuhs@yandex.ru>
2023-01-29 12:01:34 +0300

    fix delete all-data

--- src/api/controllers/testing.all.data.controller.ts
+++ src/api/controllers/testing.all.data.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Delete } from '@nestjs/common';
-import mongoose from 'mongoose';
+import { Controller, Delete, Res } from '@nestjs/common';
+import { BlogService } from '../../application/services/blog.service';
+import { PostService } from '../../application/services/post.service';
+import { UserService } from '../../application/services/user.service';
+import { Response } from 'express';
 
 @Controller('testing/all-data')
 export class TestingAllDataController {
+  constructor(
+    protected blogService: BlogService,
+    protected postService: PostService,
+    protected userService: UserService,
+  ) {}
   @Delete()
-  async DeleteAll() {
-    const collections = mongoose.connection.collections;
-    for (const key in collections) {
-      await collections[key].deleteMany({});
-    }
+  async DeleteAll(@Res() res: Response) {
+    await this.blogService.deleteAllBlogs();
+    await this.postService.deleteAllPosts();
+    await this.userService.deleteAllUsers();
+    return res.sendStatus(204);
   }
 }
